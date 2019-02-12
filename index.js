da342e5ac36b088e275ded30cb51e1489bbcccf2
Reid Mitchell <reidmitchell1@gmail.com>
2019-02-11 22:51:22 -0800

    Put settings in JSON file

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ const { Canvas } = require('./Canvas');
 const { Pixer } = require('./Pixer');
 const { randomInt } = require('./utils');
 
-const settings = {
-  canvasWidth: 640,
-  canvasHeight: 480,
-  maxFrames: Infinity,
-  untilFilled: true,
-  pixerCount: 30,
-  colorPalette: [[2, 10, 181], [250, 250, 255], [200, 200, 255], [5, 50, 255], [220, 230, 255]]
-};
+const settings = require('./settings.json');
 
 const start = Date.now();
 console.log('Starting...');
@@ -23,7 +16,7 @@ for (let i = 0; i < settings.pixerCount; i++) {
 
   let r, g, b;
   if (settings.colorPalette && settings.colorPalette.length) {
-    const colorIndex = randomInt(0, settings.colorPalette.length);
+    const colorIndex = randomInt(0, settings.colorPalette.length - 1);
     [r, g, b] = settings.colorPalette[colorIndex];
   } else {
     [r, g, b] = [randomInt(0, 255), randomInt(0, 255), randomInt(0, 255)];
@@ -40,7 +33,7 @@ while (true) {
   frameCount++;
 
   if (settings.untilFilled && canvas.filled) break;
-  if (frameCount > settings.maxFrames) break;
+  if (settings.maxFrames && frameCount > settings.maxFrames) break;
 }
 
 canvas.saveToFile(`out/test-${start}.png`);
